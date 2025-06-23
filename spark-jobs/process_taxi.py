fd28c2520e3e09875db1d5937b253ea38ac2d706
Lê Xuân Mạnh <108165360+lexmanh@users.noreply.github.com>
2025-06-23 09:09:10 +0700

    upgrade latest

--- spark-jobs/process_taxi.py
+++ spark-jobs/process_taxi.py
@@ -1,6 +1,5 @@
 from pyspark.sql import SparkSession
-# from delta import configure_spark_with_delta_pip
-from delta.pip_utils import configure_spark_with_delta_pip
+from delta import configure_spark_with_delta_pip
 
 # Initialize Spark with Delta Lake and Hive Metastore
 builder = SparkSession.builder.appName("ProcessTaxiData") \
@@ -11,7 +10,24 @@ builder = SparkSession.builder.appName("ProcessTaxiData") \
     .config("spark.hadoop.fs.s3a.secret.key", "password") \
     .config("spark.hadoop.fs.s3a.path.style.access", "true") \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-    .config("spark.hive.metastore.uris", "thrift://hive-metastore:9083")
+    .config("spark.hive.metastore.uris", "thrift://hive-metastore:9083") \
+    .config("spark.hadoop.fs.s3a.impl.disable.cache", "true") \
+    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
+    .config("spark.hadoop.fs.s3a.security.credential.provider.path", "") \
+    .config("spark.hadoop.hadoop.security.authentication", "simple") \
+    .config("spark.hadoop.hadoop.security.authorization", "false") \
+    .config("spark.hadoop.java.security.krb5.debug", "true") \
+    .config("spark.hadoop.ipc.client.fallback-to-simple-auth-allowed", "true") \
+    .config("spark.hadoop.hadoop.rpc.protection", "simple") \
+    .config("spark.hadoop.hadoop.security.authentication.simple.anonymous.enable", "true") \
+    .config("spark.hadoop.auth_to_local", "") \
+    .config("spark.hadoop.dfs.namenode.kerberos.principal", "") \
+    .config("spark.hadoop.dfs.datanode.kerberos.principal", "") \
+    .config("spark.hadoop.yarn.resourcemanager.principal", "") \
+    .config("spark.hadoop.mapreduce.jobhistory.principal", "") \
+    .config("spark.hadoop.hadoop.kerberos.kinit.command", "/bin/false") \
+    .config("spark.hadoop.fs.hdfs.impl.disable.cache", "true") # Disable cache for HDFS as well, just in case
+
 spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
 # Read CSV from MinIO
